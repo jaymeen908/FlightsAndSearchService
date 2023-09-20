0f281f2dd5a6987faa94de327fbe519bb2ddd250
Jaymeen Patel <pjaymeen908@gmial.com>
2023-09-20 13:15:48 +0530

    Added unique constraint and started cityrepostiory

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+
 const { PORT } = require('./config/serverConfig');
 
 const setupAndStartServer = async () => {
@@ -7,12 +8,15 @@ const setupAndStartServer = async () => {
     // create the express object
     const app = express();
 
-    app.use(bodyParser.json);
+    app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
     
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`server started at ${PORT}`);
-        console.log(process.env);
+        // await city.create({
+        //     name:"New Delhi", 
+        // })
+       
     })
 }
 
